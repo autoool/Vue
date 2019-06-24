2d2311dd1e26825de40f4dd79480b429bf4fdcec
chaozaiai <chaozaiai@163.com>
2019-06-25 00:14:01 +0800

    first

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// import "ant-design-vue/dist/antd.less";
+import { Button } from "ant-design-vue";
+
+// Vue.component(Button.name, Button);
 
 Vue.config.productionTip = false;
+
+Vue.use(Button);
 
 new Vue({
   router,
